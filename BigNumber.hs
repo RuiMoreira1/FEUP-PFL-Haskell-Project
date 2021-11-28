8dadedf4b44fc297e1f066e896b5ada4de159d3f
N0il <sergio.r.gama@gmail.com>
2021-11-28 11:45:47 +0000

    done fibLista, fibs for BN, safeDivBN and commented BigNumber and Fib modules, missing tests and readme, and a bug in sub

--- BigNumber.hs
+++ BigNumber.hs
@@ -1,45 +1,43 @@
+module BigNumber (BigNumber (..), scanner, output, somaBN, subBN, mulBN, divBN, notBN) where
+
 import Data.Char(digitToInt, intToDigit)
 import Distribution.Compat.Lens (_1)
+import Data.Bifunctor ( Bifunctor(bimap) )
 
+{- BigNumber Definition (2.1) -}
 data BigNumber = BN Bool [Int] | Zero deriving (Show)
 
+{- Util functions -}
+
+-- Converts a String to a list of Ints
 stringToNumbers :: String -> [Int]
 stringToNumbers [] = []
 stringToNumbers digits = digitToInt(head digits) : stringToNumbers(tail digits)
 
+-- Converts a list of Ints into a String
 numbersToString :: [Int] -> String
 numbersToString [] = []
 numbersToString digits = intToDigit(head digits) : numbersToString(tail digits)
 
+-- Removes trailling "0"'s from a String
 trimString :: String -> String
 trimString [] = "0"
 trimString s | head s == '0' = trimString (tail s)
              | otherwise = s
 
+-- Reduces a list of signs into one
 trimSigns :: String -> String
 trimSigns s | length s == 1 && head s == '+' = "+"
             | length s == 1 && head s == '-' = "-"
             | otherwise = if head s /= head (tail s) then trimSigns('-':drop 2 s) else trimSigns ('+':drop 2 s)
 
---not used yet, but may be needed
+-- Removes the trailling 0's from a list of Ints
 trimInts :: [Int] -> [Int]
 trimInts [] = [0]
 trimInts l | head l == 0 = trimInts (tail l)
            | otherwise = l
 
-scanner :: String -> BigNumber
-scanner s |  trimString s == "0" || '+':xs == "+0" || '-':xs == "-0" = Zero
-          | head s == '-' = BN False (stringToNumbers xs)
-          | head s == '+' = BN True (stringToNumbers xs)
-          | otherwise = error "Input value following this sintax: [+|-]*[0|1|2|3|4|5|6|7|8|9]*"
-          where xs = trimString (tail s)
-
-output :: BigNumber -> String
-output Zero = "0"
-output (BN False digits) = "-" ++ trimString (numbersToString digits)
-output (BN True digits) = "+" ++ trimString (numbersToString digits)
-
-{-} Helper function to check wether BigNumber a > BigNUmber b 
+{- Compares two BigNumbers (a and b) and returns:
 0 -> if a > b 
 1 -> if b > a
 2 -> if a == b -}
@@ -53,29 +51,21 @@ biggerBN (BN _ digitsA) (BN _ digitsB)
     | length digitsB > length digitsA = 1
     | head digitsA > head digitsB = 0
     | head digitsB > head digitsA = 1
-    | otherwise = biggerBN (BN True (tail digitsA)) (BN True (tail digitsB)) {-} In case the heads are equal compare the next number -}
+    | otherwise = biggerBN (BN True (tail digitsA)) (BN True (tail digitsB)) {- In case the heads are equal compare the next number -}
 
+-- Negates a BigNumber
 notBN :: BigNumber -> BigNumber
 notBN Zero = Zero
 notBN (BN sign digits) = BN (not sign) digits
 
-somaBN :: BigNumber -> BigNumber -> BigNumber
-somaBN Zero Zero = Zero
-somaBN Zero (BN sign digits) = BN sign digits
-somaBN (BN sign digits) Zero = BN sign digits
-somaBN (BN False digitsA) (BN False digitsB) = notBN (BN True (somaBNaux 0 digitsA digitsB))             {-} -a + (-b) <=> -a - b <=> -(a+b) -}
-somaBN (BN True digitsA) (BN True digitsB) = BN True (somaBNaux 0 digitsA digitsB)                       {-} a + b  -}
-somaBN (BN True digitsA) (BN False digitsB) = subBN (BN True digitsA) (BN True digitsB)                  {-} a + (-b) <=> a - b -}
-somaBN (BN False digitsA) (BN True digitsB) = subBN (BN True digitsB) (BN True digitsA)                  {-} -a + b <=> b - a -}
-
-{-} One strategy would be giving the function the big number list already reversed
+{- One strategy would be giving the function the BigNumber list already reversed
 and using head and tail, but he have the init and last functions that to the exact 
-opposite of the head and tail, therefore not needing the function in reverse order -}
+opposite of the head and tail, therefore there is no need to reverse the order of the list. -}
 
-{-} The recursive function call with the where condition, is used to manage the increments
-, given the sum of the last 2 indexes of the big number, if the sum is >= 10, we calculate use the 
-div function that returns the integer value of those two numbers divided, given the increment for the next
-operation and we concatenate to the end of the list, the mod operation of the sum -}
+{- The recursive function call with the where condition is used to manage the increments. 
+Given the sum of the last 2 indexes of the BigNumber, if the sum is >= 10, we use the 
+div function that returns the value of those two numbers divided, giving the increment for the next
+operation. Finaly, we concatenate to the end of the list, the mod operation of the sum. -}
 
 somaBNaux :: Int -> [Int] -> [Int] -> [Int]
 somaBNaux 0 [] b = b
@@ -83,31 +73,17 @@ somaBNaux 0 a [] = a
 somaBNaux inc [] [] = [inc]
 somaBNaux inc a [] = somaBNaux (div sum' 10) (init a) [] ++ [mod sum' 10]
     where
-        sum' = last a + inc 
+        sum' = last a + inc
 somaBNaux inc [] b = somaBNaux (div sum'' 10) [] (init b) ++ [mod sum'' 10]
     where
         sum'' = last b + inc
-somaBNaux inc a b = somaBNaux (div sum''' 10) (init a) (init b) ++ [mod sum''' 10] {-} Recursive call to aux function, with carry management-}
-    where 
+somaBNaux inc a b = somaBNaux (div sum''' 10) (init a) (init b) ++ [mod sum''' 10] {- Recursive call to aux function, with carry management -}
+    where
         sum''' = last a + last b + inc
-
-subBN :: BigNumber -> BigNumber -> BigNumber
-subBN Zero Zero = Zero
-subBN bn Zero = bn
-subBN Zero (BN sign digits) = BN (not sign) digits
-subBN (BN True digitsA) (BN True digitsB)                                                               {-} a - b  -}
-    | result == 2 || result == 4 = Zero                                                                 {-} If b > a do b - a and the negate the result 100 - 120 = -(120-100)-}
-    | result == 0 = BN True (subBNaux 0 digitsA digitsB)
-    | result == 1 = notBN (BN True (subBNaux 0 digitsB digitsA)) 
-    | otherwise = error "Bad comparison"
-    where result = biggerBN (BN True digitsB) (BN True digitsA)  
-subBN (BN False digitsA) (BN False digitsB) = subBN (BN True digitsB) (BN True digitsA)                 {-} -a - (-b) <=> -a + b <=> b -a  -}                                                       
-subBN (BN True digitsA) (BN False digitsB) = somaBN (BN True digitsA) (BN True digitsB)                 {-} a - (-b) <=> a + b -}
-subBN (BN False digitsA) (BN True digitsB) = notBN (somaBN (BN True digitsB) (BN True digitsA))        {-} -a - b <=> - (a+b) -}
 
 subBNaux :: Int -> [Int] -> [Int] -> [Int]
 subBNaux 0 [] b = b
-subBNaux 0 a [] = a 
+subBNaux 0 a [] = a
 subBNaux inc [] [] = [inc]
 subBNaux inc a [] = subBNaux  (abs (div sub' 10)) (init a) [] ++ [mod sub' 10]
     where sub' = last a - inc
@@ -116,32 +92,80 @@ subBNaux inc [] b = subBNaux  (abs (div sub'' 10)) [] (init b) ++ [mod sub'' 10]
 subBNaux inc a b = dropWhile(==0) (subBNaux (abs (div sub''' 10)) (init a) (init b) ++ [mod sub''' 10])
     where sub''' = last a - inc - last b
 
-mulBN :: BigNumber -> BigNumber -> BigNumber
-mulBN Zero _ = Zero
-mulBN _ Zero = Zero
-mulBN (BN False digitsA) (BN True digitsB) = notBN (BN True (mulBNaux digitsA digitsB))                                    {-} -a * b <=> - (a*b) -}
-mulBN (BN True digitsA) (BN False digitsB) = notBN (BN True (mulBNaux digitsA digitsB))                                    {-} a * -b <=> - (a*b) -}
-mulBN (BN True digitsA) (BN True digitsB) = BN True (mulBNaux digitsA digitsB)                                             {-} a * b -}
-mulBN (BN False digitsA) (BN False digitsB) = BN True (mulBNaux digitsA digitsB)                                           {-} -a * -b <=> a *b -}
-
 mulBNaux :: [Int] -> [Int] -> [Int]
 mulBNaux [] _ = []
 mulBNaux _ [0] = []
 mulBNaux _ [] = []
 mulBNaux digitsA digitsB = somaBNaux 0 digitsA (mulBNaux digitsA (subBNaux 0 digitsB [1]))
 
-divBN :: BigNumber -> BigNumber -> (BigNumber, BigNumber)
-divBN Zero _ = (Zero,Zero)
-divBN _ Zero = error "Diving by Zero" 
-divBN (BN False digitsA) (BN True digitsB) = let y = divBNaux [] digitsA digitsB in (BN False (fst y), BN True (snd y)) 
-divBN (BN True digitsA) (BN True digitsB) = let y = divBNaux [] digitsA digitsB in (BN True (fst y), BN True (snd y))
-divBN (BN True digitsA) (BN False digitsB) = let y = divBNaux [] digitsA digitsB in (BN False (fst y), BN True (snd y))
-divBN (BN False digitsA) (BN False digitsB) = let y = divBNaux [] digitsA digitsB in (BN True (fst y), BN True (snd y))
-    
 divBNaux :: [Int] -> [Int] -> [Int] -> ([Int],[Int])
 divBNaux _ _ [0] = error "Diving by Zero"
 divBNaux _ [0] _ = ([0],[0])
-divBNaux a digitsA digitsB 
+divBNaux a digitsA digitsB
     | biggerBN (BN True digitsA) (BN True digitsB) == 1 = (a, digitsA)
     | otherwise = divBNaux (somaBNaux 0 a [1]) list digitsB
     where list = subBNaux 0 digitsA digitsB
+
+{- BigNumber Functions -}
+
+--2.2: scanner - Converts a String into BigNumber 
+scanner :: String -> BigNumber
+scanner s |  trimString s == "0" || '+':xs == "+0" || '-':xs == "-0" = Zero
+          | head s == '-' = BN False (stringToNumbers xs)
+          | head s == '+' = BN True (stringToNumbers xs)
+          | otherwise = error "Input value following this sintax: [+|-]*[0|1|2|3|4|5|6|7|8|9]*"
+          where xs = trimString (tail s)
+
+--2.2: output - Converts a BigNumber into a String
+output :: BigNumber -> String
+output Zero = "0"
+output (BN False digits) = "-" ++ trimString (numbersToString digits)
+output (BN True digits) = "+" ++ trimString (numbersToString digits)
+
+--2.3: Adds two BigNumbers together
+somaBN :: BigNumber -> BigNumber -> BigNumber
+somaBN Zero Zero = Zero
+somaBN Zero (BN sign digits) = BN sign digits
+somaBN (BN sign digits) Zero = BN sign digits
+somaBN (BN False digitsA) (BN False digitsB) = notBN (BN True (somaBNaux 0 digitsA digitsB))                        {-} (-a) + (-b) <=> -a-b <=> -(a+b)-}
+somaBN (BN True digitsA) (BN True digitsB) = BN True (somaBNaux 0 digitsA digitsB)                                  {-} (+a) + (+b) <=> a+b            -}
+somaBN (BN True digitsA) (BN False digitsB) = subBN (BN True digitsA) (BN True digitsB)                             {-} (+a) + (-b) <=> a-b            -}
+somaBN (BN False digitsA) (BN True digitsB) = subBN (BN True digitsB) (BN True digitsA)                             {-} (-a) + (+b) <=> b-a            -}
+
+--2.4: Subtracts two BigNumbers
+subBN :: BigNumber -> BigNumber -> BigNumber
+subBN Zero Zero = Zero
+subBN bn Zero = bn
+subBN Zero (BN sign digits) = BN (not sign) digits
+subBN (BN True digitsA) (BN True digitsB)                                                                           {-} (+a) - (+b) <=> a-b           -}
+    | result == 2 || result == 4 = Zero                            {-} If a==b -> a-b=0 -}
+    | result == 0 = BN True (subBNaux 0 digitsA digitsB)           {-} If a>b -> (a-b)  -}
+    | result == 1 = notBN (BN True (subBNaux 0 digitsB digitsA))   {-} If b>a -> -(b-a) -}
+    | otherwise = error "Error on BiggerBN comparison"
+    where result = biggerBN (BN True digitsB) (BN True digitsA)
+subBN (BN False digitsA) (BN False digitsB) = subBN (BN True digitsB) (BN True digitsA)                             {-} (-a) - (-b) <=> -a+b <=> b-a  -}
+subBN (BN True digitsA) (BN False digitsB) = somaBN (BN True digitsA) (BN True digitsB)                             {-} (+a) - (-b) <=> a+b           -}
+subBN (BN False digitsA) (BN True digitsB) = notBN (somaBN (BN True digitsB) (BN True digitsA))                     {-} (-a) - (+b) <=> -(a+b)        -}
+
+--2.5: Multiplies two BigNumber 
+mulBN :: BigNumber -> BigNumber -> BigNumber
+mulBN Zero _ = Zero
+mulBN _ Zero = Zero
+mulBN (BN False digitsA) (BN True digitsB) = BN False (mulBNaux digitsA digitsB)                                    {-} (-a) * (+b) <=> -(a*b)        -}
+mulBN (BN True digitsA) (BN False digitsB) = BN False (mulBNaux digitsA digitsB)                                    {-} (+a) * (-b) <=> -(a*b)        -}
+mulBN (BN True digitsA) (BN True digitsB) = BN True (mulBNaux digitsA digitsB)                                      {-} (+a) * (+b) <=> a*b           -}
+mulBN (BN False digitsA) (BN False digitsB) = BN True (mulBNaux digitsA digitsB)                                    {-} (-a) * (-b) <=> a*b           -}
+
+--2.6: Division between two BigNumbers
+divBN :: BigNumber -> BigNumber -> (BigNumber, BigNumber)
+divBN Zero _ = (Zero,Zero)
+--divBN _ Zero = error "Diving by Zero" {- this is intentionally commented, because of the safeDivBN function existance -}
+divBN (BN False digitsA) (BN True digitsB) = divBN (BN True digitsA) (BN False digitsB)                             {-} (-a) / (+b) <=> -(a/b)        -}
+divBN (BN True digitsA) (BN True digitsB) = divBN (BN False digitsA) (BN False digitsB)                             {-} (+a) / (+b) <=> a/b           -}
+divBN (BN True digitsA) (BN False digitsB) = let y = divBNaux [] digitsA digitsB in bimap (BN False) (BN True) y    {-} (+a) / (-b) <=> -(a/b)        -}
+divBN (BN False digitsA) (BN False digitsB) = let y = divBNaux [] digitsA digitsB in bimap (BN True) (BN True) y    {-} (-a) / (-b) <=> a/b           -}
+
+--3: Division between two BigNumbers with division by 0 safeguard
+safeDivBN :: BigNumber -> BigNumber -> Maybe (BigNumber, BigNumber)
+safeDivBN _ Zero = Nothing
+safeDivBN n m = Just (divBN n m)
--- Fib.hs
+++ Fib.hs
@@ -1,15 +1,61 @@
- --Fibonacci recursive version 1
- fibRec :: (Integral a) => a -> a
- fibRec 0 = 0
- fibRec 1 = 1
- fibRec n = fibRec(n-1) + fibRec(n-2)
+import BigNumber ( BigNumber(..), somaBN, subBN )
 
---Fibonacci partial list implementation
---fibLista = zipWith (+) (1:fibLista) (0:1:fibLista)
+{- Fibonacci Integral functions -}
 
---Fibonacci infinit List implementation
- fibListaInfinita :: Int -> Integer
- fibListaInfinita n = fib !! n
-    where
-      fib = 0 : 1 : zipWith (+) fib (tail fib)
+--1.1: Fibonacci recursive naive version 
+fibRec :: (Integral a) => a -> a
+fibRec 0 = 0
+fibRec 1 = 1
+fibRec n = fibRec(n-1) + fibRec(n-2)
+
+--1.2: Fibonacci dynamic programming list implementation
+fibLista :: Integral a => a -> a
+fibLista = fibListaAux [] 2
+
+fibListaAux :: Integral a => [a] -> a -> a -> a
+fibListaAux _ _ 0 = 0
+fibListaAux _ _ 1 = 1
+fibListaAux [] _ n = fibListaAux [0, 1] 2 n
+fibListaAux lista i n
+            | i<=n = fibListaAux (lista ++ [lista !! fromIntegral(i-1) + lista !! fromIntegral(i-2)]) (i+1) n
+            | i>n = last lista
+
+--1.3: Fibonacci infinit List implementation
+fibListaInfinita :: Int -> Integer
+fibListaInfinita n = fib !! n
+  where fib = 0 : 1 : zipWith (+) fib (tail fib)
+
+{- Fibonacci BigNumber Functions -}
+
+--1.4: Fibonacci recursive naive version 
+fibRecBN :: BigNumber -> BigNumber
+fibRecBN Zero = Zero
+fibRecBN (BN _ [1]) = BN True [1]
+fibRecBN (BN _ n) = somaBN (fibRecBN n1) (fibRecBN n2)
+    where n1 = subBN (BN True n) (BN True [1])
+          n2 = subBN (BN True n) (BN True [2])
+
+-- Converts Int into a list of Ints
+intToList :: Int -> [Int]
+intToList 0 = []
+intToList n =  intToList x ++ [n `mod` 10]
+    where x = n `div` 10
+
+--1.5: Fibonacci dynamic programming list implementation
+fibListaBN :: Int -> BigNumber
+fibListaBN = fibListaAuxBN [] 2
+
+fibListaAuxBN :: [BigNumber] -> Int -> Int -> BigNumber
+fibListaAuxBN _ _ 0 = Zero
+fibListaAuxBN _ _ 1 = BN True [1]
+fibListaAuxBN [] _ n = fibListaAuxBN [Zero, BN True [1]] 2 n
+fibListaAuxBN lista i n
+            | i<=n = fibListaAuxBN (lista ++ [ somaBN (lista !! fromIntegral(i-1))  (lista !! fromIntegral(i-2))]) (i+1) n
+            | i>n = last lista
+
+--1.6: Fibonacci infinit List implementation
+fibListaInfinitaBN :: Int  -> BigNumber
+fibListaInfinitaBN n = fib !! n
+  where fib = Zero : BN True [1] : zipWith somaBN fib (tail fib)
+
 
